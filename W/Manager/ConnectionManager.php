3ce8f9020c0888a21ee9a1639260d63a3d7f8d1b
gsylvestre <gsylvestre@gmail.com>
2015-06-29 07:09:54 +0200

    move database connection to static singleton manager

---
+++ W/Manager/ConnectionManager.php
@@ -0,0 +1,35 @@
+<?php
+
+	namespace W\Manager;
+
+	use \PDO;
+	use \PDOException;
+
+	class ConnectionManager
+	{
+
+		private static $dbh;
+
+		public static function getDbh()
+		{
+			if (!self::$dbh){
+				self::setNewDbh();
+			}
+			return self::$dbh;
+		}
+
+		public static function setNewDbh()
+		{
+			try {
+			    //connexion à la base avec la classe PDO et le dsn
+			    self::$dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS, array(
+			        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", //on s'assure de communiquer en utf8
+			        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //on récupère nos données en array associatif par défaut
+			        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING         //on affiche les erreurs. À modifier en prod. 
+			    ));
+			} catch (PDOException $e) { //attrappe les éventuelles erreurs de connexion
+			    echo 'Erreur de connexion : ' . $e->getMessage();
+			}
+		}
+
+	}
--- W/Manager/Manager.php
+++ W/Manager/Manager.php
@@ -2,8 +2,6 @@
 
 	namespace W\Manager;
 
-	use \PDO;
-	use \PDOException;
 
 	abstract class Manager 
 	{
@@ -14,21 +12,7 @@
 		public function __construct()
 		{
 			$this->setTableFromClassName();
-			$this->setDbh();
-		}
-
-		private function setDbh()
-		{
-		    try {
-		        //connexion à la base avec la classe PDO et le dsn
-		        $this->dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS, array(
-		            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", //on s'assure de communiquer en utf8
-		            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //on récupère nos données en array associatif par défaut
-		            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING         //on affiche les erreurs. À modifier en prod. 
-		        ));
-		    } catch (PDOException $e) { //attrappe les éventuelles erreurs de connexion
-		        echo 'Erreur de connexion : ' . $e->getMessage();
-		    }
+			$this->dbh = ConnectionManager::getDbh();
 		}
 
 		private function setTableFromClassName()
