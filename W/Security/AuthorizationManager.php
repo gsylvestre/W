71446a0d228b7a2818417dc6d7d46e5fda087873
gsylvestre <gsylvestre@gmail.com>
2015-07-12 14:50:22 +0200

    simpler test in isGranted

--- W/Security/AuthorizationManager.php
+++ W/Security/AuthorizationManager.php
@@ -17,13 +17,7 @@
 
 		public function isGranted($role)
 		{
-
-			if (empty($this->user["role"])){
-				return false;
-			}
-
-
-			if ($this->user["role"] === $role){
+			if (!empty($this->user["role"]) && $this->user["role"] === $role){
 				return true;
 			}
 
