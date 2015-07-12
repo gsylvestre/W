9b93049517e908fe26f26746f27d90a61e13e27b
gsylvestre <gsylvestre@gmail.com>
2015-07-12 16:16:59 +0200

    change config order

--- app/config.php
+++ app/config.php
@@ -3,17 +3,17 @@
 	//url
 	define("W_BASE_URL", "/formation/w");
 
-	//user authentification
-	define("W_DB_USER_TABLE", "users");
-	define("W_DB_USERNAME_PROPERTY", "username");
-	define("W_DB_EMAIL_PROPERTY", "email");
-	define("W_DB_PASSWORD_PROPERTY", "password");
-
 	//database
     define("W_DB_HOST", "localhost");  
     define("W_DB_USER", "root");     
     define("W_DB_PASS", "");         
     define("W_DB_NAME", "w"); 
     define("W_DB_TABLE_PREFIX", "");
+
+	//user authentification
+	define("W_DB_USER_TABLE", "users");
+	define("W_DB_USERNAME_PROPERTY", "username");
+	define("W_DB_EMAIL_PROPERTY", "email");
+	define("W_DB_PASSWORD_PROPERTY", "password");
 
     require("routes.php");
