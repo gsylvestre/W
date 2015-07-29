b9b908c4caa1626b464ef42404ab5f8c52ae5881
guillaumewf3 <guillaumewf3@gmail.com>
2015-07-29 09:33:32 +0200

    remove wrong methods

--- W/Security/StringUtils.php
+++ W/Security/StringUtils.php
@@ -5,37 +5,14 @@
 	class StringUtils 
 	{
 
-		public static function hashPassword($plainPassword)
+		public static function randomString($length = 80)
 		{
-			return password_hash($plainPassword, PASSWORD_DEFAULT);
-		}
-
-		public static function randomString($length = 32)
-		{
-			$possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			$possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-";
 	        $factory = new \RandomLib\Factory;
 			$generator = $factory->getGenerator(new \SecurityLib\Strength(\SecurityLib\Strength::MEDIUM));
 			$string = $generator->generateString($length, $possibleChars);
 
 	        return $string;
-		}
-
-		public static function stringEquals($string1, $string2)
-		{
-			if (function_exists('hash_equals')) {
-				return hash_equals($string1, $string2);
-			}
-
-			if(strlen($str1) != strlen($str2)) {
-				return false;
-			} 
-			$res = $str1 ^ $str2;
-			$ret = 0;
-			for($i = strlen($res) - 1; $i >= 0; $i--){
-				$ret |= ord($res[$i]);
-			}
-			return !$ret;
-			
 		}
 
 	}
