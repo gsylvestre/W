2f34d1b54a8a6a50545aa0a9d3defda034d876d4
guillaumewf3 <guillaumewf3@gmail.com>
2015-07-29 11:01:39 +0200

    add dummy index in root folder, with some help

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-	//see index.php in "public" directory
-	die();
+
+	//you should create a vhost pointing directly inside public/ directory
+	//or point your domain there
+
+	//See 'public/' directory
+	header('HTTP/1.0 403 Forbidden');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Wrong.</title>
+</head>
+<body>
+	<a href="public/">Try here, maybe</a>
+</body>
+</html>
