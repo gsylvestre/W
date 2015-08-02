62f59f26fd570d597dcc0b15301b25d517ed6802
gsylvestre <gsylvestre@gmail.com>
2015-08-02 14:07:41 +0200

    add comments

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
-
+	//autochargement des classes
 	require("../vendor/autoload.php");
+
+	//configuration
 	require("../app/config.php");
 	
+	//instancie notre appli en lui passant les routes
 	$app = new W\App($w_routes);
+	
+	//exécute l'appli
 	$app->run();
