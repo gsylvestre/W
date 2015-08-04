3f53a3b4a2822625a3c6f2b367a9147516eca363
gsylvestre <gsylvestre@gmail.com>
2015-08-04 08:33:03 +0200

    add flash message support

--- W/Session/SessionManager.php
+++ W/Session/SessionManager.php
@@ -19,7 +19,7 @@
 		public function start()
 		{
 			if(!isset($_SESSION)) {
-			     session_start();
+			    session_start();
 			}
 		}
 
@@ -66,6 +66,76 @@
 		public function destroy()
 		{
 			session_destroy();
+		}
+
+
+		/**
+		 * Ajoute un message flash
+		 * @param string $message Le message flash
+		 * @param string $type    Le type de message (notice, error, etc.)
+		 */
+		public function addFlash($message, $type)
+		{
+			//ajoute un tableau vide si c'est le premier flash
+			if (!$this->get('flashes')){
+				$this->set('flashes', []);
+			}
+
+			//ajoute ce type si non-présent
+			$flashes = $this->get('flashes');
+			if (!isset($flashes[$type])){
+				$flashes[$type] = [];
+			}
+
+			//ajoute le message
+			$flashes[$type][] = $message;
+		}
+
+		/**
+		 * Récupère les messages flash, et les supprime
+		 * @param  string $type Type de messages flash à récupérer
+		 * @return mixed       Les messages, ou null
+		 */
+		public function getFlashes($type = null)
+		{
+			if (!$this->hasFlash()){
+				return null;
+			}
+
+			$flashes = $this->get("flashes");
+
+			if (!$type){
+				$this->unset("flashes");
+				return $flashes;
+			}
+
+			if ($this->hasFlash($type)){
+				unset($_SESSION['flashes'][$type]);
+				return $flashes[$type];
+			}
+
+			return null;
+		}
+
+		/**
+		 * Vérifie si des messages flash sont présents
+		 * @param  string  $type Le type de message à vérifier
+		 * @return boolean       True si présent, false sinon 
+		 */
+		public function hasFlash($type = null)
+		{
+			$flashes = $this->get('flashes');
+			if (!$flashes){
+				return false;
+			}
+			if (!$type && !empty($flashes)){
+				return true;
+			}
+			if ($type && array_key_exists($type, $flashes) && !empty($flashes[$type])){
+				return true;
+			}
+
+			return false;
 		}
 
 	}
