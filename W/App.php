37ca5650a1d1a1ab5539d56475de7c58ac404857
gsylvestre <gsylvestre@gmail.com>
2015-08-10 18:01:19 +0200

    add lotsa comments

--- W/App.php
+++ W/App.php
@@ -4,14 +4,17 @@ namespace W;
 
 class App 
 {
-
+	/** @var array Contient le tableau de configuration complet */
 	protected $config;
+	/** @var \AltoRouter Le routeur */
 	protected $router;
 
 	/**
-	 * Constructeur, recevant un array de routes
+	 * Constructeur
+	 * @param array $w_routes Tableau de routes
+	 * @param array $w_config Tableau optionnel de configurations
 	 */
-	public function __construct(array $w_config, array $w_routes)
+	public function __construct(array $w_routes, array $w_config = array())
 	{
 		session_start();
 		$this->setConfig($w_config);
@@ -20,27 +23,34 @@ class App
 		$this->router->addRoutes($w_routes);
 	}
 
-	private function setConfig($w_config)
+	/**
+	 * Récupère les configurations fournies par l'appli
+	 * @param array $w_config Tableau de configuration
+	 */
+	private function setConfig(array $w_config)
 	{
 		$defaultConfig = [
 			//url
-			'base_url' => '',
+			'base_url' => '',								//chemin relatif au domaine, menant à la racine de l'appli
 
-		   	//database connection infos
-			'db_host' => 'localhost',
-		    'db_user' => 'root',
-		    'db_pass' => '',
-		    'db_name' => '',
-		    'db_table_prefix' => '',
+		   	//information de connexion à la bdd
+			'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
+		    'db_user' => 'root',							//nom d'utilisateur pour la bdd
+		    'db_pass' => '',								//mot de passe de la bdd
+		    'db_name' => '',								//nom de la bdd
+		    'db_table_prefix' => '',						//préfixe ajouté aux noms de table
 
-			//user authentification
-			'security_user_table' => 'users',
-			'security_username_property' => 'username',
-			'security_email_property' => 'email',
-			'security_password_property' => 'password',
-			'security_role_property' => 'role',
+			//authentification, autorisation
+			'security_user_table' => 'users',				//nom de la table contenant les infos des utilisateurs
+			'security_username_property' => 'username',		//nom de la colonne pour le "pseudo"
+			'security_email_property' => 'email',			//nom de la colonne pour l'"email"
+			'security_password_property' => 'password',		//nom de la colonne pour le "mot de passe"
+			'security_role_property' => 'role',				//nom de la colonne pour le "role"
+
+			'security_login_route_name' => 'login',			//nom de la route affichant le formulaire de connexion
 		];
 
+		//remplace les configurations par défaut par celle de l'appli
 		$this->config = array_merge($defaultConfig, $w_config);
 	}
 
