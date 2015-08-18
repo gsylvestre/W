5c2406b3765ac4b67c6241dacf4b1361c13119e4
gsylvestre <gsylvestre@gmail.com>
2015-08-18 13:54:58 +0200

    fix 404 bad title and typos in readme

--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,5 +1,5 @@
-<?php $this->layout('layout', ['title' => 'Créer un nouvel administrateur']) ?>
+<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>404. Lost?</h1>
+<h1>404. Perdu ?</h1>
 <?php $this->stop('main_content'); ?>
