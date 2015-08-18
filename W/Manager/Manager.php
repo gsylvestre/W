576a3d15d4ca684d9e264c43db2bc7f05c7ae096
gsylvestre <gsylvestre@gmail.com>
2015-08-18 18:47:39 +0200

    secure order clauses in findAll

--- W/Manager/Manager.php
+++ W/Manager/Manager.php
@@ -87,6 +87,16 @@ abstract class Manager
 
 		$sql = "SELECT * FROM " . $this->table;
 		if (!empty($orderBy)){
+
+			//sécurisation des paramètres, pour éviter les injections SQL
+			if(!preg_match("#^[a-zA-Z0-9_$]+$#", $orderBy)){
+				die("invalid orderBy param");
+			}
+			$orderDir = strtoupper($orderDir);
+			if($orderDir != "ASC" && $orderDir != "DESC"){
+				die("invalid orderDir param");
+			}
+
 			$sql .= " ORDER BY $orderBy $orderDir";
 		}
 		$sth = $this->dbh->prepare($sql);
