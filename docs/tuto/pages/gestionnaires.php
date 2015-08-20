b61a5e36c8f4ac61e89f5e4bbfcd4b2dac1e15d0
gsylvestre <gsylvestre@gmail.com>
2015-08-20 11:32:09 +0200

    add managers page and some conventions

--- docs/tuto/pages/gestionnaires.php
+++ docs/tuto/pages/gestionnaires.php
@@ -1,11 +1,54 @@
 <h2>Les gestionnaires</h2>
-	<h3>À quoi servent les gestionnaires ?</h3>
-	<h3>Comment créer un nouveau gestionnaire ?</h3> 
-		La class W\Manager\Manager
-	<h3>Les méthodes héritées disponibles</h3>
-		find()
-		findAll()
-		delete()
-		insert()
-		update()
-		setTable()
+<h3>À quoi servent les gestionnaires ?</h3>
+<p>Les gestionnaires (ou <span class="code">Manager</span>) sont les classes responsables d'exécuter les requêtes à votre base de données. C'est le coeur de votre "modèle".</p>
+<p>Concrètement, chaque fois que vous souhaitez faire une requête à la base de données, vous devriez venir y créer une fonction qui s'en chargera, sauf si elle existe déjà dans le gestionnaire de base du framework.</p>
+<h3>Comment créer un nouveau gestionnaire ?</h3> 
+<p>Dans votre application, vous pourriez avoir un gestionnaire par table MySQL (sans obligation). Chacune de ces classes devraient hériter de <span class="code">\W\Manager\Manager</span>, le gestionnaire de base du framework, qui vous fera profiter de quelques méthodes utiles pour les requêtes simples à la base de données.</p>
+
+<p>Par exemple, pour créer un gestionnaire relié à une table de commentaires nommées <span class="code">comments</span> : </p>
+<pre><code>&lt;?php /* app/Manager/CommentManager.php */
+namespace Manager;
+
+class CommentManager extends \W\Manager\Manager 
+{
+	//Récupère les commentaires associés à un article
+	public function findPostComments($postId)
+	{
+		//...
+	}
+}
+</code></pre>
+
+
+<h3>Les propriétés et méthodes héritées du Manager</h3>
+<p>Voici les propriétés et les méthodes les plus utiles, héritées du gestionnaire de base. Vous devrez créer vos propres méthodes pour réaliser toutes les requêtes SQL plus complexes !</p>
+<pre><code>/* W/Manager/Manager.php */
+
+//propriété contenant le nom de la table (deviné grâce au nom de votre gestionnaire)
+protected $table;
+
+//connexion à la base de données
+protected $dbh;
+
+// Définit le nom de la table (si le nom déduit ne convient pas)
+public function setTable($table)
+
+
+// Récupère une ligne de la table en fonction d'un identifiant
+public function find($id)
+
+// Récupère toutes les lignes de la table
+public function findAll($orderBy = "", $orderDir = "ASC")
+
+// Efface une ligne en fonction de son identifiant
+public function delete($id)
+
+// Ajoute une ligne
+//Le premier argument est un tableau associatif de valeurs à insérer
+public function insert(array $data, $stripTags = true)
+
+// Modifie une ligne en fonction d'un identifiant
+// Le premier argument est un tableau associatif de valeurs à insérer
+// Le second est l'identifiant de la ligne à modifier
+public function update(array $data, $id, $stripTags = true)
+</code></pre>
